c86e85553971176161ff97c1559d523ce3050e35
JityaBusi <119723522+JityaBusi@users.noreply.github.com>
2026-03-13 22:17:58 +0530

    Update UploadImage.jsx

--- src/components/UploadImage.jsx
+++ src/components/UploadImage.jsx
@@ -15,7 +15,7 @@ function UploadImage() {
     try {
       setLoading(true);
       const res = await axios.post(
-        "http://localhost:5000/api/media/upload",
+        "https://verification-media-backend.onrender.com/api/media/upload",
         formData
       );
       setResult(res.data);
